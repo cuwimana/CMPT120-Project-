49c2c6738e07059c48385677498820f46e3e15f3
cuwimana <charlotte.uwimana1@notes.marist.eu>
2017-09-15 14:06:16 -0400

    change introdauction and ending on Project

--- Project.py
+++ Project.py
@@ -1,8 +1,22 @@
+#Projet 1
+# Author: Charlotte Uwimana
 print(" Welcome to Hidden Box Game!!!")
 print()
-print (" We live in world of dreams, imagination, challenges, and fear. We like to dream big and to have unlimited wishes. Every morning, we always woke up without knowing what will happen tomorrow. We start to think about what do for the whole day to make our dreams come true. But there one thing we do not consider, life is like a puzzle game. It is made of small part but some of them are ignored. One thing, we should know there always a hidden box in life that can open our future. However, ignoring these small parts, it can make it harder to get the box…… Imagine one day find yourself in your dream school. You have never been there. Around you, there are many buildings and you are curious to get inside them, but there is only one which will give you access to them. This is your time to find the access....\n"
+print (" We live in world of dreams, imagination, challenges, and fear. We like to dream big and to have unlimited wishes. Every morning, we woke up without knowing what will happen tomorrow. We start to think about what do for the day to make our dreams come true. But there one thing we do not consider, life is like a puzzle game. It is a big box made by small parts. But sometime, we ignored those parts and still want to get that big box. How? One thing, we should know there always a hidden box in life that has the key for our future. However, ignoring these small parts, it can make it harder to get that key…… Imagine one day find yourself in your dream school. You have never been there. Around you, there are many buildings and you are curious to get inside them. Unfortunately there is only one thing which will give you access to them. This is your time to find the access....\n"
        "===========================================================================\n")
 
+print (" You are now in parking lot, the is one house infront of you")
+Next= input("Press Enter to contunue")
+
+print ("you are next to door, door is closed")
+Next= input ("Press Enter to contunue")
+
+print ("you are standing near the person, ask her what you want")
+Next =input("Press Enter to Contunue")
+
+print (" there are, dictionary, maps, calender")
+next= ( "press enter tp contunue")
+print()
 Western= "Open the box" 
 start = input("press enter to continue .. ")
 while True:
