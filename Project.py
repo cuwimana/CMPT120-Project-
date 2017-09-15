f38cb37fad2e0f3f26f1afda85f2e5e065ce0cfb
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-09-14 23:58:31 -0400

    change Introduction and Ending on Project.py

--- Project.py
+++ Project.py
@@ -1,20 +1,13 @@
-tile = "write the title of the game here"
-intro = "Write the introduction of the game here -- backstory"
+print(" Welcome to Hidden Box Game!!!")
+print()
+print (" We live in world of dreams, imagination, challenges, and fear. We like to dream big and to have unlimited wishes. Every morning, we always woke up without knowing what will happen tomorrow. We start to think about what do for the whole day to make our dreams come true. But there one thing we do not consider, life is like a puzzle game. It is made of small part but some of them are ignored. One thing, we should know there always a hidden box in life that can open our future. However, ignoring these small parts, it can make it harder to get the box…… Imagine one day find yourself in your dream school. You have never been there. Around you, there are many buildings and you are curious to get inside them, but there is only one which will give you access to them. This is your time to find the access....\n"
+       "===========================================================================\n")
 
-player = ""
-initial_loation = ""
-player_score = 0
-
-location1 = "Describe location 1"
-location2 = "Describe location 2"
-location3 = "Describe location 3"
-location4 = "final"
-
+Western= "Open the box" 
 start = input("press enter to continue .. ")
 while True:
     new_location = input("Which direction do you want to go?")
-    
-    if new_location == location4:
-        print("game conclusion story...")
-        print("Display copyright, name, and email")
+    if new_location == Western:
+        print("Wow!!!! you got ID to access all buildings")
+        print("Copyright (c), Charlotte Uwimana, and charlotte.uwimana1@notes.marist.edu")
         break
