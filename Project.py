991016174c8607e42c4f8726be2d3a8bf3a6e6be
cuwimana <charlotte.uwimana1@notes.edu>
2017-09-28 17:57:19 -0400

    Final virison of project 1, resolves #1 and #2

--- Project.py
+++ Project.py
@@ -1,53 +1,95 @@
 #Projet 1
 # Author: Charlotte Uwimana
-print(" Welcome to Hidden Box Game!!!")
+# September 15 2017
+
+title = (" Magic Place\n"
+         "=====================\n")
+
+backStory = ("In every walk with nature, you can receive far more that you seek or lose everything."
+             "You can discover fun, mysterious and adventurous places that you will never forget. One"
+             "day you wake up in the morning and walk slowly to enjoy the freedom and nature of the"
+             "forest that you used to enjoy hiking. At this moment, things have changed. You are discovering"
+             "magic places you have never seen in your life as well as facing many challenges. This will be"
+             "your unforgettable moment in your life discovering a magic island that prisoned"
+             "you under the museum curve of Greek arts and crafts.......")
+            
+forest = ("You are walking in tannin-brown forest.  The grass is crispy under your feet. The trees are"
+          "skyscraper tall. Hares are scampering away from you up ahead. The morning stars are shining"
+          "like sliver snowflakes. The peace of the morning is soul soothing. The forest’s smell is fresh"
+          "and organic. You can only feel joy in your heart flourishing like bubble in boiled water."
+          "But be careful, this forest is not safe.")
+
+prisonHouse =("You are stuck in prison house, in the middle of nowhere. You are caught by rebels, and they"
+              "have taken all your properties. No way to communicate with your family or to escape. There"
+              "is a box in the corn of the prison house. This is your time to find a way to escape.")
+
+lakeBank = ("You arrive to the bank of lake. There is a small boat, swimming vest. There is an island"
+            "in the east of the lake. It is your time to decide how you can use these materials to enjoy"
+            "your exploration. You can either go to explore island or stay and enjoy the nature of the lake.")
+
+island = ("This is a beautiful island, you have ever visited. It is like a fairy-tale garden.Its beaches are"
+          "covered with soft golden sands. It has an ocean flowers and exotic trees. It has a museum of Greek arts and"
+          "crafts. Add to this playful sun’s rays.You can get an unbelievable tint of piece and beauty. The sight is so"
+          "marvelous and relaxing that even the worst thoughts and the most gloomy mood disappear in no time. It is time"
+          "to explore either Museum or enjoy the beaty of beach.")
+
+beach = ("You arrive at the beach. You can hear the sea roaring. You can feel the wind blowing your face."
+         "The beach is so soothing and calm. It is time to get more relaxing, a building a sandcastle,"
+         "and lots of other things to have fun.")
+
+museum = ("This is house of magic and museum for ancient Greek arts and crafts. You can hear a stranger voice telling:"
+          "I am the most interesting person you have ever met, maybe. I am going to tell you my stories and my experiences."
+          "I may inspire and you can learn from me. I am a museum full of Greek arts. Are you starting to be nervous and wonder"
+          "how museum can speak? I told you this is a magic place. Continue to enjoy, you will explore more")
+prompt =("Press Enter to continue.\n")
+end = "Ooops! All museum doors are locked themselves. Good bye real world, I am stuck in the Museum!"
+copyRight =  "Copyright(c), Charlotte Uwimana, and charlotte.uwimana1@notes.marist.edu"
+score = 0
+
+print(title)
 print()
-print (" We live in world of dreams, imagination, challenges, and fear. We like to dream big and to have unlimited wishes. Every morning, we woke up without knowing what will happen tomorrow. We start to think about what do for the day to make our dreams come true. But there one thing we do not consider, life is like a puzzle game. It is a big box made by small parts. But sometime, we ignored those parts and still want to get that big box. How? One thing, we should know there always a hidden box in life that has the key for our future. However, ignoring these small parts, it can make it harder to get that key…… Imagine one day find yourself in your dream school. You have never been there. Around you, there are many buildings and you are curious to get inside them. Unfortunately there is only one thing which will give you access to them. This is your time to find the access....\n"
-       "===========================================================================\n")
-def main():
-    score=0
-    locat1=" Arrived on compus. You are in parking lot where you can see all beauty of the compus. You start to feel very impressive anc curious. There many buildings around you but you do not where to go."
-    locat2=" The path leads you to your doom. Ahead in that path, you can see a toller build. As you walk toward the building there other two buidings. all of them are closed."
-    locat3= " You are standing next to the toller building. Outside door is not locked."
-    locat4= " No one is around you. There is only empy desk in front of you and closed door. Trying to open door. No access!!" 
-    locat5= " you are downstairs in  toller building. There people eating and you start to feel hungry. No access!!!"
-    locat6= " your talking to your advisor. Ask her what you want?"
-    locat7= " your standing infront of the comera. You card is almost done." 
-    
-    print( " Hidden Box Start here\n"
-           "Initial score: ", score, "\n")
-    Next= input( "Press Enter to contunue")
-    print("location:", locat1, "\n")
-    score= score+5
-    print(" way to go, your score:", score, "\n")
-    Next= input("Press Enter to contunue.")
+print(backStory)
+print("Welcome to Magic Place game!!! \n")
+print ("Initial score: ", score, "\n")
+input(prompt)
 
-    print("location:", locat2, "\n")
-    score=score+5
-    print(" your score:", score,"\n")
-    Next= input(" Press Enter to contue.")
-    
-    print("location:",locat3,"\n")
-    score=score+5
-    print("your score:", score,"\n")
-    Next=input( "Press Enter to contue.")
-    
-    print("location:",locat4,"\n")
-    score=score+5
-    print("score:", score,"\n")
-    Next=input( "Press Enter to contue.")
-    
-    print("location:",locat5,"\n")
-    score=score+5
-    print(" your score:", score,"\n")
-    Next=input( "Press Enter to contue.")
-    
-    print("location:",locat6,"\n")
-    score=score+5
-    print("score:", score,"\n")
-    Next=input( "Press Enter to contue.")
+curLocation = forest
+score= score + 5
+print(curLocation )
+print(" Your score is:", score, "\n")
+input(prompt)
 
-    print("Wow!!!! you got ID to access all buildings")
-    score=score
-    print("Total score:" , score,"\n")
-    print("Copyright (c), Charlotte Uwimana, and charlotte.uwimana1@notes.marist.edu")
+curLocation = prisonHouse
+score= score + 5
+print(curLocation, "\n")
+print(" Your score is:", score, "\n")
+input(prompt)
+
+curLocation = lakeBank
+score= score + 5
+print(curLocation, "\n")
+print("Your score is:", score, "\n")
+input(prompt)
+
+curLocation = island
+score= score + 5
+print(curLocation, "\n")
+print("Your score is:", score, "\n")
+input(prompt)
+
+curLocation = beach
+score= score + 5
+print (curLocation, "\n")
+print("Your score is:", score, "\n")
+input(prompt)
+
+curLocation = museum
+score= score + 5
+print(curLocation, "\n")
+print("Your score is:", score, "\n")
+input(prompt)
+      
+print(end)
+score=score
+print("Total score:" , score,"\n")
+print(copyRight)
