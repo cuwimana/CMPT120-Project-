7262a5c017a85580f179cc5373ba80971343a286
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-10-25 21:46:59 -0400

    add improvement on project2, resolves #7.

--- Project.py
+++ Project.py
@@ -30,37 +30,38 @@ def intro():
 def locations():
     
     forest = ("                FOREST\n"
-        "You are walking in tannin-brown forest.  The grass is crispy under your feet. The trees are"
-          "skyscraper tall. Hares are scampering away from you up ahead. The morning stars are shining"
-          "like sliver snowflakes. The peace of the morning is soul soothing. The forest’s smell is fresh"
-          "and organic. You can only feel joy in your heart flourishing like bubble in boiled water."
+        "You are walking in tannin-brown forest. The grass is crispy under your feet. The trees are "
+          "skyscraper tall. Hares are scampering away from you up ahead. The morning stars are shining "
+          "like sliver snowflakes. The peace of the morning is soul soothing. The forest’s smell is fresh "
+          "and organic. You can only feel joy in your heart flourishing like bubble in boiled water. "
           "But be careful, this forest is not safe. There are strange creatures haunting you. You must get out now...")
 
     prisonHouse =("           PRISON HOUSE\n"
                 "You are stuck in prison house, in the middle of nowhere. You are caught by creatures, and they "
-                  "have taken all your properties. No way to communicate with your family or to escape. There"
-                  " is a tunnel in the north corner of the prison house. This is your time to find a way to escape.")
+                  "have taken all your properties. No way to communicate with your family or to escape. There "
+                  "is a tunnel in the north corner of the prison house. This is your time to find a way to escape.")
     
     lakeBank = ( "              LAKE BANK\n"
                 "You arrive to a bank of a long lake. There is a small boat, swimming vest. There is an island "
                 "in the north of the lake. It is your time to decide how you can use these materials to enjoy "
-                "your exploration. You can either go to the island or stay and enjoy the nature of the lake and risk your life")
+                "your exploration. You can either go to the island or stay and enjoy the nature of the lake and risk your life.")
     
     island = ("                 ISLAND\n"
-            "You arrive at the most beautiful island you have ever visited. It is like a fairy-tale garden.Its beaches are"
-              "covered with soft golden sands. It has an ocean flowers and exotic trees. It has a museum of Greek arts and"
-              "crafts. Add to this playful sun’s rays.You can get an unbelievable tint of piece and beauty. The sight is so"
-              "marvelous and relaxing that even the worst thoughts and the most gloomy mood disappear in no time. It is time"
+            "You arrive at the most beautiful island you have ever visited. It is like a fairy-tale garden.Its beaches are "
+              "covered with soft golden sands. It has an ocean flowers and exotic trees. It has a museum of Greek arts and "
+              "crafts. Add to this playful sun’s rays.You can get an unbelievable tint of piece and beauty. The sight is so "
+              "marvelous and relaxing that even the worst thoughts and the most gloomy mood disappear in no time. It is time "
               "to either move to the Museum or enjoy the beauty of beach. Just so you know, it will get lonely...")
     
     city = ("                   CITY\n"
-            "You arrive at a city where the museam is located. The city is right at the beach You can hear the sea roaring. You can feel the wind blowing your face."
+            "You arrive at a city where the museam is located. The city is right at the beach You can "
+             "hear the sea roaring. You can feel the wind blowing your face. "
              "The city is so perfect and calm. It is time to get more relaxing, a building a sandcastle,"
-             "and lots of other things to have fun. Remember, for you to escape the mystical world, you must get to the museum")
+             "and lots of other things to have fun. Remember, for you to escape the mystical world, you must get to the museum ")
     
     museum = ("                  MUSEUM\n"
-            "This is house of magic and museum for ancient Greek arts and crafts. You can hear a stranger voice telling:"
-              "I am the most interesting person you have ever met, maybe. I am going to tell you my stories and my experiences."
+            "This is house of magic and museum for ancient Greek arts and crafts. You can hear a stranger voice telling: "
+              "I am the most interesting person you have ever met, maybe. I am going to tell you my stories and my experiences. "
               "I may inspire and you can learn from me. I am a museum full of Greek arts. Are you starting to be nervous and wonder "
               "how museum can speak? I told you this is a magic place. Continue to enjoy, you will explore more."
               "Here, you can learn about ways of getting back to your family.")
@@ -85,29 +86,29 @@ def main():
     print(current_location)
     score = 0
     while True:
-        print('\n')
-        print(name, 'your current score is:', score, "\n" )
+        print("\n")
+        print(name, "your current score is:", score, "\n" )
         move = input("Enter a direction: ").strip().lower()
-        print('\n')
-        if move in ['north', 'east', 'south', 'west']:
+        print("\n")
+        if move in ["north", "east", "south", "west"]:
             if current_location == forest:
-                if move in ['south', 'west', 'north']:
+                if move in ["south", "west", "north"]:
                     print("You are still in the forest.")
-                elif move == 'east':
-                    print('You are captured by wild creatures and put in Prison!\n')
+                elif move == "east":
+                    print("You are captured by wild creatures and put in Prison!\n")
                     current_location = prisonHouse
                     if current_location not in visited:
                         visited.append(current_location)
                         score+=5
                     print(current_location)
             elif current_location == prisonHouse:
-                if move in ['east', 'south', 'west']:
-                    print('You bump into the prison wall.\n')
-                if move == 'west':
+                if move in ["east", "south", "west"]:
+                    print("You bump into the prison wall.\n")
+                if move == "west":
                     print("Going back into the forest!\n")
                     current_location = forest
                     print(current_location)
-                if move == 'north':
+                if move == "north":
                     print("you have arrive at a shore of a sea!\n")
                     current_location = lakeBank
                 if current_location not in visited:
@@ -115,13 +116,13 @@ def main():
                     score+=5
                     print(current_location)
             elif current_location == lakeBank:
-                if move in ['east','west']:
-                    print('you are still on the lake bank!\n')
-                elif move == 'south':
-                    print('Going back into the forest!\n')
-                    current_location = forest
+                if move in ["east","west"]:
+                    print("you are still on the lake bank!\n")
+                elif move == "south":
+                    print("Going back into the prison house!\n")
+                    current_location = prisonHouse
                     print(current_location)
-                elif move == 'north':
+                elif move == "north":
                     print("you are now at an island!\n")
                     current_location = island 
                     if current_location not in visited:
@@ -129,23 +130,27 @@ def main():
                         score+=5
                     print(current_location)
             elif current_location == island:
-                if move in ['east', 'west']:
+                if move in ["north", "east"]:
                     print("you are still on the middle of the sea!\n")
-                elif move == 'south':
+                elif move == "south":
                     print("you are going back into the lake bank!\n")
                     current_location = lakeBank
                     print(current_location)
-                elif move == 'north':
-                    print('you are now in the mystical city\n')
+                elif move == "west":
+                    print("you are now in the mystical city\n")
                     current_location = city
                     if current_location not in visited:
                         visited.append(current_location)
                         score+=5
                     print(city)
             elif current_location == city:
-                if move in ['north', 'west', 'south']:
+                if move in ["north","south"]:
                     print("You are wandering around the city\n")
-                elif move == 'east':
+                elif move in ["east"]:
+                    print("You are going back to the island")
+                    current_location = island
+                    print(current_location)
+                elif move == "west":
                     score+=5
                     current_location = museum
                     print(current_location)
