9915e1dd3dea8f785f9b7e59d3355878166c403c
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-10-27 14:02:29 -0400

    Add more comment in code

--- Project.py
+++ Project.py
@@ -1,10 +1,8 @@
 
-                    
-
 
 #Projet 1
 # Author: Charlotte Uwimana
-# September 15 2017
+# October 27 2017
 
 import time 
 
@@ -16,9 +14,9 @@ def intro(): # showing introduction of the game, title and backstory and return 
       "While on the forest walking along the beautiful nature,"
       "you can receive far more that you seek or lose everything."
       "You can discover fun, mysterious and adventurous things that you will never forget."
-      "You realize you are in a magic places you have never seen before and wanted to escape to the real world"
-      "But you can't unless you get to a meseum on the other side of the sea"
-      "Your job is to get to that museum.......")
+      "You realize you are in a magic places you have never seen before and wanted to escape to the real world "
+      "But you can't unless you get to a meseum on the other side of the sea. "
+      "Your job is to get to that museum before five minutes......." )
     print(title+"\n")
     name = input("Enter your name please to continue: ")
     print('\n')
@@ -39,12 +37,12 @@ def locations(): # holding list discriptions and lacation valiables signed to in
                   "But be careful, this forest is not safe. There are strange creatures haunting you. You must get out now...")
               , ("           PRISON HOUSE\n"
                   "You are stuck in prison house, in the middle of nowhere. You are caught by creatures, and they "
-                  "have taken all your properties. No way to communicate with your family or to escape. There "
-                  "is a tunnel in the north corner of the prison house. This is your time to find a way to escape.")
+                  "have taken all your properties. No way to communicate with your family or to escape. There  "
+                  "is a tunnel in the north corner of the prison house. This is your time to find a way to escape. ")
               , ("              LAKE BANK\n"
-                  "You arrive to a bank of a long lake. There is a small boat, swimming vest. There is an island "
-                  "in the north of the lake. It is your time to decide how you can use these materials to enjoy "
-                  "your exploration. You can either go to the island or stay and enjoy the nature of the lake and risk your life.")
+                  "You arrive to a bank of a long lake. There is a small boat, swimming vest. There is an island  "
+                  "in the north of the lake. It is your time to decide how you can use these materials to enjoy  "
+                  "your exploration. You can either go to the island or stay and enjoy the nature of the lake and risk your life. ")
               , ("                 ISLAND\n"
                   "You arrive at the most beautiful island you have ever visited. It is like a fairy-tale garden.Its beaches are "
                   "covered with soft golden sands. It has an ocean flowers and exotic trees. It has a museum of Greek arts and "
@@ -54,24 +52,24 @@ def locations(): # holding list discriptions and lacation valiables signed to in
               , ("                   CITY\n"
                   "You arrive at a city where the museam is located. The city is right at the beach You can "
                   "hear the sea roaring. You can feel the wind blowing your face. "
-                  "The city is so perfect and calm. It is time to get more relaxing, a building a sandcastle,"
+                  "The city is so perfect and calm. It is time to get more relaxing, a building a sandcastle, "
                   "and lots of other things to have fun. Remember, for you to escape the mystical world, you must get to the museum ")
               , ("                  MUSEUM\n"
                   "This is house of magic and museum for ancient Greek arts and crafts. You can hear a stranger voice telling: "
                   "I am the most interesting person you have ever met, maybe. I am going to tell you my stories and my experiences. "
                   "I may inspire and you can learn from me. I am a museum full of Greek arts. Are you starting to be nervous and wonder "
-                  "how museum can speak? I told you this is a magic place. Continue to enjoy, you will explore more."
-                  "Here, you can learn about ways of getting back to your family.")
+                  "how museum can speak? I told you this is a magic place. Continue to enjoy, you will explore more. "
+                  "Here, you can learn about ways of getting back to your family. ")
               , ("                     SMALL TUNNEL\n" 
-                  "You are moving under a small tunnel surrounded by big rocks. It really dark and quiet."
-                  "You can only hear the sound of your feet and the beat of your heart. It is scary and"
-                  "you do not know where you will end up. You can either go back or continue your journey.")
+                  "You are moving under a small tunnel surrounded by big rocks. It really dark and quiet. "
+                  "You can only hear the sound of your feet and the beat of your heart. It is scary and "
+                  "you do not know where you will end up. You can either go back or continue your journey. ")
               , ( "                          CAVE\n"
-                  "The path halted and a cave appeared. Ivy wound round the cave and conceal the entrance that is a jagged opening."
-                  "Inside is dim although being lit by two small fires in the corner of the cave. In the Centre is a small roasting"
-                  "pot and in the far north corner there is a small woven mat made with dried grass. Cave is dank and the only sound"
-                  "you can hear is the dripping water and wind from lake in east of cave. In west there is a small tunnel,"
-                  "which may lead you to exciting places.")
+                  "The path halted and a cave appeared. Ivy wound round the cave and conceal the entrance that is a jagged opening. "
+                  "Inside is dim although being lit by two small fires in the corner of the cave. In the Centre is a small roasting "
+                  "pot and in the far north corner there is a small woven mat made with dried grass. Cave is dank and the only sound "
+                  "you can hear is the dripping water and wind from lake in east of cave. In west there is a small tunnel, "
+                  "which may lead you to exciting places. ")
                 ]
     forest = 0
     prisonHouse = 1
@@ -83,7 +81,6 @@ def locations(): # holding list discriptions and lacation valiables signed to in
     cave = 7
     return descriptions, forest, prisonHouse, lakeBank, island, city, museum, tunnel, cave  
 def conclude(score, name): # show copyright and show scores 
-   
     end = "\nOoops! All museum doors are locked themselves. Good bye real world, I am stuck in the Museum!"
     copyRight =  "Copyright(c), @Charlotte Uwimana, charlotte.uwimana1@notes.marist.edu"
     print(end+"\n")
@@ -92,9 +89,9 @@ def conclude(score, name): # show copyright and show scores
     print(copyRight)
 
 def main():
-   
+    
     descriptions, forest, prisonHouse, lakeBank, island, city, museum, tunnel, cave = locations() # retuned from locations fuction
-    # map and help variables 
+    # map and help 
     ask_help = "You can only move north, east, south or west from your current location. Enter the directions to move towards." 
     map = (" \n Museum --------- City ----------Island  \n" 
            " \n    |                               |    \n"
@@ -108,11 +105,11 @@ def main():
     visited = [current_location] # tracking locations that have been visited 
     print(descriptions[current_location])
     score = 0
-    start_time = time.time()
-    stop_time = time.time() # variable for tracking start time for the game    
+    start_time = time.time() # variable for tracking start time for the gem
+    stop_time = time.time() # variable for tracking end time for the game    
     while True: # loop for the game 
         stop_time = time.time() # variable for tracking end time 
-        if (stop_time - start_time) >= 300: # test if time is less than or equal five minute, then stop
+        if (stop_time - start_time) >= 300: # check whether the player is running out of time
             print( " You ran out of time!\n")
             print("Your score is:", score, "\n")
             return 
@@ -129,10 +126,10 @@ def main():
                 elif move == "north":
                     print("You are in the way lead you to the cave!\n")
                     current_location = cave
-                if current_location not in visited:
-                    visited.append(current_location)
-                    score+=5
-                print(descriptions[current_location])
+                if current_location not in visited: # check whether current location has been visited or not
+                    visited.append(current_location) # add in visited list
+                    score+=5 # add 5 on the score 
+                print(descriptions[current_location]) 
             elif current_location == cave: # track movement in the cave
                 if move == "north":
                     print("you are still in the cave!\n")
