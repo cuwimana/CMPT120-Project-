d73d4d01b70f33c7e99ec2dbdba36e42a72ea47f
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-11-26 21:35:24 -0500

    location class

---
+++ location.py
@@ -0,0 +1,27 @@
+# Project5
+# Author: Charlotte UWIMANA
+# 1 December 2017
+class Locale(object):
+	#docstring for locale
+	# place name
+	name = ""
+	# place description before
+	desc = ""
+	# place description after
+	new_desc = ""
+	# false if not visited, true otherwise
+	visited = False
+	# false when not search, true otherwise
+	searched = False
+	# list of items at a location
+	items = []
+	def __init__(self, name, desc, items=[]):
+		self.name = name
+		self.des = desc
+		self.items =items
+	# player drop an item to this place
+	def drop(self, item):
+		self.items.append(item)
+	# player take an item from this place
+	def take(self, item):
+		self.inventory.remove(item)
