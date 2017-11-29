b4f72d76aa1218e724d891deb2af7c91b460ea43
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-11-29 00:30:05 -0500

    use Player class to create getUserInput function, resolves #25

--- Project.py
+++ Project.py
@@ -20,7 +20,7 @@ def locations():
 				location.Locale( "the Lake Bank",( "            LAKE BANK\n"
                   "You arrive to a bank of a long lake. There are many places that you can enjoy around the lake."
                  "But you need some items that can help you to do so. It is your time to decide how you can find those items and places "
-                 " to help you explore more. Or you can stay and enjoy the nature of the lake and risk your life...\n "), ["life Jacket", "Boat"]), 
+                 " to help you explore more. Or you can stay and enjoy the nature of the lake and risk your life...\n "), ["Boat"]), 
 				location.Locale( "the Island",    ("                 ISLAND\n"
                   "You arrive at the most beautiful island you have ever visited. It is like a fairy-tale garden.Its beaches are "
                   "covered with soft golden sands. It has an ocean flowers and exotic trees. It has a museum of Greek arts and "
@@ -37,7 +37,7 @@ def locations():
                   "\"I am the most interesting person you have ever met, maybe. I am going to tell you my stories and my experiences. "
                   "I may inspire you and you can learn from me. I am a museum full of Greek arts.\" Are you starting to be nervous and wonder "
                   "how museum can speak? I told you this is a magic place. Continue to enjoy, you will explore more. "
-                  "Here, you can learn about ways of getting back to your family...\n "), [None]), 
+                  "Here, you can learn about ways of getting back to your family...\n "), ["Rock"]), 
 				location.Locale("the small Tunnel", ("            SMALL TUNNEL\n" 
                   "You are moving under a small tunnel surrounded by big rocks. It really dark and quiet. "
                   "You can only hear the sound of your feet and the beat of your heart. It is scary and "
@@ -54,6 +54,35 @@ def locations():
                  "and this place is not safe especially in the night...\n"), [None]), 
 				location.Locale( "the beach",("                          BEACH\n "
                  "You are now enjoying the beauty of the beach. The beach smells fresh, almost like a new ocean breeze air freshener."
-                 "The sand is hot and looks like gold blended in with little white specks...\n"),[None])
+                 "The sand is hot and looks like gold blended in with little white specks...\n"),["Blanket"])
 				]
 	return locations_list
+
+def getUserInput(): 
+    name = input("Enter your name please to continue: ") #input name
+    return player.Player(name)
+
+# showing introduction of the game, title and backstory
+#takes user name as argument
+def intro(name): 
+    #game title and intro
+    title = (        "\n              Magic Place\n"
+                 "      ===========================\n")
+    backStory = ("You mysteriously found yourself in the middle of a forest. "
+                 "While on the forest walking along the beautiful nature, "
+                 "you can receive far more that you seek or lose everything. "
+                 "You can discover fun, mysterious and adventurous things that you will never forget."
+                 "You then realize that you are in a magic places you have"
+                 "never seen before and wanted to escape to the real world "
+                 "But you can't unless you get to a meseum on the other side of the sea... "
+                 "Your job is to get to the museum with one specific item before five minutes." )
+    #printing out title and intro
+    print(title)
+    print() 
+    print("Welcome to Magic Place game "+name+"!!!")
+    print('\n')
+    print(backStory+"\n")
+    input("Press Enter to continue")
+
+player1 = getUserInput()
+intro(player1.name)
