e021e555c8d2f09c4baf6fb1a42dd2f4c3f74a6c
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-12-04 12:18:47 -0500

    add conditions for using the items, resolves#28

--- Project.py
+++ Project.py
@@ -18,7 +18,7 @@ def locations():
                   "You are stuck in prison house, in the middle of nowhere. You are caught by creatures, and they "
                   "have taken all your properties. No way to communicate with your family or to escape. There  "
                   "is a tunnel in the north corner of the prison house. This is your time to find a way to escape.\n "), ["key"]) 
-				,   location.Locale( "the Lake Bank",( "            LAKE BANK\n"
+				,   location.Locale( "the lake bank",( "            LAKE BANK\n"
                   "You arrive to a bank of a long lake. There are many places that you can enjoy around the lake."
                  "But you need some items that can help you to do so. It is your time to decide how you can find those items and places "
                  " to help you explore more. Or you can stay and enjoy the nature of the lake and risk your life...\n "), ["boat", "life jacket"]) 
@@ -38,7 +38,7 @@ def locations():
                   "\"I am the most interesting person you have ever met, maybe. I am going to tell you my stories and my experiences. "
                   "I may inspire you and you can learn from me. I am a museum full of Greek arts.\" Are you starting to be nervous and wonder "
                   "how museum can speak? I told you this is a magic place. Continue to enjoy, you will explore more. "
-                  "Here, you can learn about ways of getting back to your family...\n "), ["rock"])
+                  "Here, you can learn about ways of getting back to your family...\n "), [])
 				,   location.Locale("the small tunnel", ("            SMALL TUNNEL\n" 
                   "You are moving under a small tunnel surrounded by big rocks. It really dark and quiet. "
                   "You can only hear the sound of your feet and the beat of your heart. It is scary and "
@@ -48,21 +48,20 @@ def locations():
                   "Inside is dim although being lit by two small fires in the corner of the cave. In the Centre is a small roasting "
                   "pot and in the far north corner there is a small woven mat made with dried grass. Cave is dank and the only sound "
                   "you can hear is the dripping water and wind from lake in east of cave. In west there is a small tunnel, "
-                  "which may lead you to exciting places...\n "), [ "mate" ])
+                  "which may lead you to exciting places...\n "), [ "mate", "wax box" ])
 				,   location.Locale(" the restaurant",("                          RESTAURANT\n"
                  "It is now becoming darker. You are walking toward a fancy restaurant in the whole city. It is a beautiful place full "
                  "of all kinds of food, wins and beers. Enjoy but be careful you. Things here are so expensive "
                  "and this place is not safe especially in the night...\n"), ["food box"]) 
 				,   location.Locale( "the beach",("                          BEACH\n "
                  "You are now enjoying the beauty of the beach. The beach smells fresh, almost like a new ocean breeze air freshener."
-                 "The sand is hot and looks like gold blended in with little white specks...\n"),["Blanket", "ticket"])
+                 "The sand is hot and looks like gold blended in with little white specks...\n"),["blanket", "ticket"])
                                 ,   location.Locale("the amusement park", ("                 AMUSEMENT PARK \n"
                  " You are arrived in amusement park. You can enjoy the roll cost, and beautiful nature, and people around you. "
                  " However, make sure that you get insurance to do so.\n"), ["insurance"])
-                                ,   location.Locale(" the water fall", ("                      WATER FALL\n "   
-                 " the waterfall is quarium-blue, dizzling onto the rock. At its widest point, it is surging and plinging down "
-                 " the mountain. It has a beutiful serenity-pool at the botton. you thrown yourself under the waterfall and the" 
-                 " coldness of it start make you shuddering. Find the way to get there.\n"), [])
+                                ,   location.Locale(" the water fall", ("                      WATERFALL\n "   
+                 " It is quarium-blue, dizzling onto the rock. At its widest point, it is surging and plinging down "
+                 " the mountain. It has a beutiful serenity-pool at the botton. Now, you can enjoy swimming or go back.\n"), [])
                                 ,   location.Locale(" the bridge", ( "                        BRIDGE \n" 
                  " You are standing near the bridge that can lead you to the beautiful island in the west.\n"), [])
 
@@ -72,7 +71,7 @@ def locations():
 # get user input and return name
 #no arguments
 def getUserInput(): 
-    name = input("Enter your name please to continue: ") #input name
+    name = input("Enter your name please to continue: ")#input name
     return player.Player(name)
 
 # showing introduction of the game, title and backstory
@@ -81,31 +80,27 @@ def intro(name):
     #game title and intro
     title = (        "\n              Magic Place\n"
                  "      ===========================\n")
-    backStory = ("You mysteriously found yourself in the middle of a forest. "
-                 "While on the forest walking along the beautiful nature, "
-                 "you can receive far more that you seek or lose everything. "
+    backStory = ("All things in life are not granted. The choice you make every day are the ones that lead you to the future." 
+                 + name+ ", mysteriously found yourself in the middle of a forest. While in this forest, "
+                 "You then realize that you are in a magic places you have never seen before."
                  "You can discover fun, mysterious and adventurous things that you will never forget."
-                 "You then realize that you are in a magic places you have"
-                 "never seen before and wanted to escape to the real world "
-                 "But you can't unless you get to a meseum on the other side of the sea... "
-                 "Your job is to get to the museum with one specific item before five minutes." )
+                 " And this is where your journey started....!")
+    task = ( name + ", your task in this game is to get to final distination, the museum and be able to enter. In you journey,"
+             "you can collect all meterials that you think that will help you in down road and to enjoy some places. You have limited"
+             "time to finish your task. Good luck!!!!!!!")
     #printing out title and intro
     print(title)
     print() 
     print("Welcome to Magic Place game "+name+"!!!")
     print('\n')
     print(backStory+"\n")
+    print(task+"\n")
     input("Press Enter to continue")
 def conclude(player1): 
-    end = ("\nOoops! All museum doors are locked themselves. You are stuck in the Museum! "
-           "You can only use the key to escape the Museum to the real world")
+    end = ("\nOoops! All museum doors are locked. But do not worry, you have corrected the key. Now you can use it to get inside ")
     copyRight =  "Copyright(c), @Charlotte Uwimana, charlotte.uwimana1@notes.marist.edu"
     print(end+"\n")
-    if "key" in player1.inventory:
-        print("Good JOb "+player1.name+", you have won the game. You can now use your key to open through the secret door at the museum to the real world.")
-    else:
-        player1.score = 0
-        print("Good JOb "+player1.name+", you tried your best to finish the game. Unfortunately, you're stuck in the museum since you did not collected the key.")
+    print("Good JOb "+player1.name+", you have won the game. You arrived the final destination")
     print("Your Total score is:" , player1.score,"\n")
     print(copyRight)
     
@@ -137,7 +132,7 @@ def matrix(current_loc, move):
         ,   [ island,     prisonHouse, cave,           beach      ]  # lake bank
         ,   [ None,       lakeBank,    city,           bridge     ]  # Island
         ,   [ restaurant, None,        museum,         island     ]  # city
-        ,   [ None,       None,        None,           None       ]  # Museum
+        ,   [ None,       tunnel,        None,         city       ]  # Museum
         ,   [ museum,     None,        None,           cave       ]  # tunnel
         ,   [ None,       forest,      tunnel,         lakeBank   ]  # cave
         ,   [ None,       city,        None,           None       ]  # restaurant
@@ -150,13 +145,9 @@ def matrix(current_loc, move):
     new_loc = world[current_loc][move]
     return new_loc
 
-# 
+# update the score and print out the current location
 def moveTo(player1,current_location):
-    #Museum is the last location
-    if current_location.name == "the museum":
-        conclude(player1)
-        return current_location
-    elif current_location.visited:
+    if current_location.visited:
         print()
         print(current_location.desc_after)
     else:
@@ -217,11 +208,38 @@ def game_loop(player1):
             else:
                 move_num = 3 
             if (matrix(current_location_index, move_num) != None):
+                #make sure boat exist if going from lank bank to island
+                if(current_location.name == "the lake bank" and 
+                   matrix(current_location_index, move_num) == 3 and 
+                   "boat" and "life jacket" not in player1.inventory):
+                    print("You need both a boat and life jacket to cross from the lake bank to the island")
+                    continue
+                # make sure that to cross the bridge a person has ticket 
+                if(current_location.name == " the bridge" and 
+                   matrix(current_location_index, move_num) == 3 and 
+                   "ticket" not in player1.inventory):
+                    print("You need to get ticket to cross the bridge and go to island.\n")
+                    continue
+                # make sure that to go to the waterFall the player has insurance and life jacket
+                if(current_location.name == "the amusement park" and 
+                   matrix(current_location_index, move_num) == 11 and 
+                   "insurance" not in player1.inventory):
+                    print("You must have insurance, if you want to go to waterfall.\n")
+                    continue
+                # the final distination, museum. No key, no enter 
+                if(matrix(current_location_index, move_num) == 5 and 
+                   "key" not in player1.inventory):
+                    print("You don't have the key to get inside the museum -->Please return back and search for the key")
+                    continue
+                
                 current_location_index = matrix(current_location_index, move_num)
                 current_location = locations_list[current_location_index]
                 update_playerLoc = moveTo(player1,current_location)
+                
                 if update_playerLoc.name == "the museum":
-                    break
+                    if "key" in player1.inventory:
+                        conclude(player1)
+                        break
             else:
                 print("You are still in",current_location.name, "!")
         
@@ -265,6 +283,7 @@ def game_loop(player1):
         # Oh you need help with movement directions?
         elif move == "help": 
             print(ask_help)
+            
         # if you haven't collected the map, you can't see it
         elif move == "map":
             if "map" in player1.inventory:
